1c34ec676bd42ed153a5d870bab9e02111495309
My name <iakylbek005@gmail.com>
2021-10-03 16:20:40 +0600

    config

--- data/config.py
+++ data/config.py
@@ -4,8 +4,8 @@ from environs import Env
 env = Env()
 env.read_env()
 
-BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
-ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
-IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
+BOT_TOKEN = '1754811598:AAHMz-bDYtfmo1T1tyZqlrqTS3KmtZA8N1Y'  # Забираем значение типа str
+ADMINS = ['874928357'] # Тут у нас будет список из админов
+IP = "localhost"  # Тоже str, но для айпи адреса хоста
 CHANNEL_ID = -1001598596296
 IS_POSTING_REQUESTEQ = False
